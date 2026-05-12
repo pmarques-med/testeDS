169681591d6dde3ee95b96084c29cc4bcf64f353
Pedro Marques <pmarques@med.up.pt>
2026-05-12 15:22:14 +0100

    Pequeno update

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,6 @@ const app = express();
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(express.json());
 
-app.listen(3000, () => console.log("Servidor (TypeORM + SQLite) a correr na porta 3000"));
+app.listen(3000, () => console.log("Servidor  a correr na porta 3000"));
 
 export default app;
